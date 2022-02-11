ca5049d8f3d2e855bd98dfd2772cd45ed9513fe3
PranavDuggal <pranavduggal1999@yahoo.com>
2022-02-11 19:39:16 +0530

    modified selenium-server file

--- Drivers/selenium-server.pl
+++ Drivers/selenium-server.pl
@@ -1,6 +1,6 @@
 use strict;
 use warnings;
 
-my @args = ("java" ,"-jar", "-Dwebdriver.chrome.driver = chromedriver.exe","selenium-server-standalone-3.141.59.jar");
+my @args = ("java" ,"-jar", "-Dwebdriver.chrome.driver = Drivers/chromedriver.exe","Drivers/selenium-server-standalone-3.141.59.jar");
 
 system(@args);
